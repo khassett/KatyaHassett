3fc655e865bf668283394f39266bc219f80e20e9
Zhenya Kovalenko <zhenya@decor48.com>
2021-11-10 06:18:58 -0800

    fixing nav on mobile

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
-$(window).on('scroll', function () {   
-  // let navbar = $(".nav");
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "0px 80px 20px 30px";
-    document.getElementById("logo").style.height = "80px";
-  } else {
-    document.getElementById("navbar").style.padding = "0px 100px 40px 100px";
-    document.getElementById("logo").style.height = "100px";
-  }
+let isMobile = false;
+isMobile = window.matchMedia("only screen and (max-width: 760px)").matches;
+
+$(window).on('scroll', function () {  
+  if ( !isMobile ) {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+      document.getElementById("navbar").style.padding = "0px 80px 20px 30px";
+      document.getElementById("logo").style.height = "80px";
+    } else {
+      document.getElementById("navbar").style.padding = "0px 100px 40px 100px";
+      document.getElementById("logo").style.height = "100px";
+    }
+  } 
 });
