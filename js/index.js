491fb534d05b8817be312681019a0b7b9790aa93
Zhenya Kovalenko <zhenya@decor48.com>
2021-11-10 06:31:41 -0800

    fixing nav

--- js/index.js
+++ js/index.js
@@ -4,11 +4,9 @@ isMobile = window.matchMedia("only screen and (max-width: 760px)").matches;
 $(window).on('scroll', function () {  
   if ( !isMobile ) {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-      document.getElementById("navbar").style.padding = "0px 80px 20px 30px";
-      document.getElementById("logo").style.height = "80px";
+      document.getElementById("navbar").style.padding = "0px 80px";
     } else {
-      document.getElementById("navbar").style.padding = "0px 100px 40px 100px";
-      document.getElementById("logo").style.height = "100px";
+      document.getElementById("navbar").style.padding = "0px 50px";
     }
   } 
 });
